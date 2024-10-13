bf67fa1768a9284bd5a1b3799acf72dacd449486
tahasajid04 <taha.sajid@xecuritypulse.com>
2024-10-13 12:16:13 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,3 @@
+music.play(music.tonePlayable(988, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+music.play(music.tonePlayable(131, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+music.play(music.tonePlayable(392, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
